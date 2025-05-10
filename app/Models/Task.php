0647c53e9937af07df2ba06091149d49b2d66eaa
rakibch <rakib9204@gmail.com>
2025-05-10 12:40:37 +0600

    removed unnecessary files

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,7 +10,7 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     //
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable,Authenticatable;
 
     public function creator()
     {
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,20 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\TaskController;
+use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+use App\Http\Controllers\TestController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     // Add your Task CRUD and assignment routes here
-    Route::apiResource('tasks', TaskController::class);
-    Route::post('/tasks/{task}/assign', [TaskController::class, 'assign']);
+    Route::get('/test-index',[TestController::class,'index']);
 
 });
